0fc451f72e9a9c6d2d9815634038393346d7dd55
kooboo-jifeng <jifeng@kooboo.com>
2014-08-05 17:14:47 +0800

    ContainerManagerExtensions

---
+++ Kooboo.Common/ObjectContainer/Dependency/IContainerManagerExtensions.cs
@@ -0,0 +1,27 @@
+﻿#region License
+// 
+// Copyright (c) 2013, Kooboo team
+// 
+// Licensed under the BSD License
+// See the file LICENSE.txt for details.
+// 
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kooboo.Common.ObjectContainer.Dependency
+{
+    public static class IContainerManagerExtensions
+    {
+        public static void AddComponent(this IContainerManager containerManager, Type service, IEnumerable<Type> implementations)
+        {
+            foreach (var item in implementations)
+            {
+                containerManager.AddComponent(service, item, item.FullName);
+            }
+        }
+    }
+}
